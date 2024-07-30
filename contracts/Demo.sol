bfd4d9fffeac85cdc0669d608daea8bcd5bfbe5c
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-07-30 15:16:42 +0300

    Demo contract

---
+++ contracts/Demo.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./AccessControl.sol";
+
+contract Demo is AccessControl {
+    bool paused;
+
+    //for better optimization it is better to assign prepared keccak256 value to constant variables
+    bytes32 public constant WITHDRAWER_ROLE =
+        keccak256(bytes("WITHDRAWER_ROLE"));
+
+    bytes32 public constant MINTER_ROLE = keccak256(bytes("MINTER_ROLE"));
+
+    constructor(address _withdrawer, address _minter) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        _grantRole(WITHDRAWER_ROLE, _withdrawer);
+        _grantRole(MINTER_ROLE, _minter);
+
+        _setRoleAdmin(MINTER_ROLE, WITHDRAWER_ROLE);
+    }
+
+    function withdraw() external onlyRole(WITHDRAWER_ROLE) {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function pause() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        paused = true;
+    }
+}
